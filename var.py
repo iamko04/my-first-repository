a3757eab68407dafcada3b315c7cbf6957338022
iamko04 <143787552+iamko04@users.noreply.github.com>
2023-09-11 16:09:21 +0900

    var.py

--- var.py
+++ var.py
@@ -1,99 +1,158 @@
-my_bar = 10
-print(my_bar)
+"""my_var = 10
+print(my_var)  
 
-#정수
+my_list = [1, 2, 3]
+print(my_list)
+print(*my_list)
+
 my_int = 10
 
-#부동 소수점
+# 부동 소수점
 my_float = 3.14
 
-#복소수
-my_complex = 3 +4j
+# 복소수
+my_complex = 3 + 4j
+print(my_int)
 
-print(my_int, my_float, my_complex)
+my_character = 'A'
+my_char = "@"
+print(my_char)
+
+my_string = 'Hello, World!'
+str_name = "python"
 
 my_bool = True
 bFlag = False
-
-print(my_bool, bFlag)
-
-my_list = [1, 2, 'three', True]
-IsElement = [3.14, "b", 'two', False]
-
-print(my_list, IsElement)
-
-print(*my_list)
+print(bFlag)
 
 my_list = [10, 3, 8, 9, 42, "hello"]
+print(my_list)
 
 print(my_list.__len__())
 
-print(my_list [0])
+my_list = [10, 3, 8, 9, 42, "hello"]
+print(my_list[3])
 
-list_el = my_list [2]
-
+list_el = my_list[2]
 print(list_el)
 
 my_list[3] = 11
+print(*my_list)
 
-print(my_list)
 
-n_add = my_list[3] + my_list[2]
-print(n_add)
+#print(my_list[3] - my_list[5])
 
-print(my_list[2:5])
 
+slice_Is = my_list[2:5]
+print(slice_Is)
 print(my_list[:3])
-
 print(my_list[0:3])
+print(my_list[4:])
 
-slice_ls = my_list[2:7]
-
-print(slice_ls)
-
-my_list = [10, 3, 8, 9, 42, "hello", slice_ls]
-
+my_list = [10, 3, 8, 9, 42, "hello", [5, 4, 2, "world"]]
 print(my_list)
+print(my_list[6][1])
+print(my_list[6][2:])
 
-my_list.insert(2, 4)
-
+my_list = [10, 3, 8, 9, 42, 8, "hello"]
 print(my_list)
+my_list.insert(2,4)
+print(*my_list)
 
 print(my_list.index(3))
 
-my_list.append(22)
-
 print(my_list)
+my_list.append(22)
+print(*my_list)
 
 print(my_list.count(8))
 
+print(my_list)
 print(my_list.pop())
+print(*my_list)
+
+my_list = [10, 3, 8, 9, 42, 8]
 
 print(my_list)
+my_list.sort()
+print(*my_list)
 
+my_list = [10, 3, 8, 9, 42, 8, "hello"]
 print(my_list)
+my_list.sort()
+print(*my_list)
 
+my_list = ['python', 'hello', 'apple']
+print(my_list)
+my_list.sort()
+print(*my_list)
+
+my_list = [10, 3, 8, 9, 42, 8, "hello"]
+print(my_list)
 my_list.reverse()
+print(*my_list)
+
+list_tmp = [5, 7, "world"]
+print(my_list, list_tmp)
+my_list.extend(list_tmp)
+print(*my_list)
+
+list_tmp = [5, 7, "world"]
+print(my_list, list_tmp)
+my_list.extend(list_tmp)
+print(*my_list)
+list_tmp.clear()
+print(list_tmp)
 
 print(my_list)
-
-my_list.extend(slice_ls)
+my_list.remove(3)
+print(*my_list)
 
 print(my_list)
-
-slice_ls.clear
-
-print(slice_ls)
-
 del my_list[2]
+print(*my_list)
 
+my_tup = (4, 2, 6, 1, "py", "w")
+print(my_tup.__len__())
+print(my_tup)
+n_add = my_tup[1] + my_tup[3]
+print(n_add)
+
+
+print(my_tup.index(2, 8, 3))
+print(my_tup.index(2, 0, 3))
+
+
+my_tup = (4, 2, 6, 1, "py", "w")
+print(my_tup.index(2, 0, 3))
+
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+my_item = my_dict.items()
+print(my_item)"""
+
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+"""dicData = {"name" : "python", "number" : 23564897}
+
+my_dict["key3"] = "value3"
+print(my_dict)
+
+my_dict.setdefault("key3")
+print(my_dict)
+
+my_dict.update({"key1" : "v4"})
+print(my_dict)
+
+del my_dict["key2"]
+print(my_dict)
+
+print("key2" in my_dict)
+print("key3" in my_dict)
+
+my_list = list(my_dict.keys())
 print(my_list)
 
-my_tuple = (1, 2, 'three', True)
-tpItem = (3.14, "b", 'two', False)
+my_set = set(my_dict.keys())
+print(my_set)"""
 
-print(my_tuple)
-print(tpItem)
-
-print(my_tuple.__len__())
-
+my_dict.clear()
+print(my_dict)
